6f6c2a69e37d9ba85d0757a6f0fce6ba7279ba54
thamiscoder <thamirisoli34@gmail.com>
2025-02-04 16:55:44 -0300

    project

---
+++ script.js
@@ -0,0 +1,16 @@
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 2000) //milisegundos = 2segundos
+
+
+function nextImage() {
+    count++; //incrementando
+    if(count>3) { 
+        count = 1; //loop
+    }
+
+    document.getElementById("radio"+count).checked = true; 
+}
